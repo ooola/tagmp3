a7e4368e53063e0e0e150f9526fb03fcc899ce01
Ola Nordstrom <ola.nordstrom@gmail.com>
2017-09-06 17:10:27 -0700

    inital test support

---
+++ test/test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# help
+./tagmp3 -h > help.out 2>&1
+diff help.out help.expected
+[[ $? -eq 0 ]] || { echo "Help test failed"; exit 1; }
+
+# test setting all tags
+cp tiny.mp3 test.mp3
+
+./tagmp3 -t abcdefghijklmnopqrstuvwxyzåäö \
+         -a ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ \
+         -l abcdefghijklmnopqrstuvwxyzåäö \
+         -g abcdefghijklmnopqrstuvwxyzåäö \
+         -k 5 \
+         -y 1990 \
+         -c abcdefghijklmnopqrstuvwxyzåäö \
+	test.mp3
+./tagmp3 -p test.mp3 > p.out 2>&1
+diff p.out p.expected
+[[ $? -eq 0 ]] || { echo "Set and print tags test failed"; exit 1; }
+
+exit 0
+
+
+
